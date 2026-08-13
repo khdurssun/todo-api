0f718f0738d79993994c726858299abe74feeb02
khsdursun <khasanovdursun018@gmail.com>
2026-08-13 21:46:15 +0500

    fixed it and added a title field.

--- database/models.py
+++ database/models.py
@@ -10,5 +10,6 @@ class Task(Base):
     __tablename__ = "tasks"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(50))
     description: Mapped[str | None] = mapped_column(String(100))
     is_completed: Mapped[bool] = mapped_column(default=False)
