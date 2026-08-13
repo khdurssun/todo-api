6210a49d6cf139836af7ac116990b5576678e0c1
khsdursun <khasanovdursun018@gmail.com>
2026-08-13 21:30:08 +0500

    added database models for table creation.

---
+++ database/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    description: Mapped[str | None] = mapped_column(String(100))
+    is_completed: Mapped[bool] = mapped_column(default=False)
