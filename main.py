205d1029b57dcf87c5c1cf6e55d2b3b5691ac693
khsdursun <khasanovdursun018@gmail.com>
2026-08-14 19:57:00 +0500

    added a welcome message to the root endpoint.

--- main.py
+++ main.py
@@ -16,5 +16,9 @@ app = FastAPI(title="Task Manager API", lifespan=lifespan)
 
 app.include_router(tasks_router)
 
+@app.get("/")
+async def main_page():
+    return {"message": "Task Manager API is ready!"}
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
