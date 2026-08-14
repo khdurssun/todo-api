e93b06bcb4f68e67afa10922fda1d963eb5aab01
khsdursun <khasanovdursun018@gmail.com>
2026-08-14 19:53:24 +0500

    imports were fixed, and an error related to the database connection URL was resolved.

--- database/database.py
+++ database/database.py
@@ -1,9 +1,9 @@
 from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from config import settings
+from database.config import settings
 
 engine = create_async_engine(
-    url=settings.database_url,
+    url=settings.DATABASE_URL,
     echo=False,
 )
 
