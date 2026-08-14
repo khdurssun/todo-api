51506b047d33899df6f709137a89da2c341de122
khsdursun <khasanovdursun018@gmail.com>
2026-08-14 19:50:27 +0500

    fixed an issue where the default value was `True` when creating or updating a task.

--- app/schemas.py
+++ app/schemas.py
@@ -5,21 +5,18 @@ class TaskBase(BaseModel):
     title: str = Field(min_length=1, max_length=50)
     description: str | None = None
     is_completed: bool = False
+    
+    model_config = ConfigDict(from_attributes=True)
 
 
 class TaskCreate(TaskBase):
     title: str = Field(min_length=1, max_length=50)
     description: str = Field(min_length=5, max_length=100)
-    is_completed: bool 
+    is_completed: bool = False
 
 
 class TaskUpdate(BaseModel):
     title: str | None = Field(min_length=1, max_length=50)
     description: str | None = None
-    is_completed: bool | None = None
+    is_completed: bool | None = False
 
-
-class Task(TaskBase):
-    id: int
-
-    model_config = ConfigDict(from_attributes=True)
