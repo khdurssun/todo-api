1a444cd2b86e4b96a98e124d0b058bc566b87d6d
khsdursun <khasanovdursun018@gmail.com>
2026-08-14 19:48:42 +0500

    code was added to `main` to connect the router and the FastAPI object, and to set up the database.

---
+++ main.py
@@ -0,0 +1,20 @@
+from contextlib import asynccontextmanager
+import uvicorn
+from fastapi import FastAPI
+
+from app.routers.task import router as tasks_router
+from database.services import create_tables
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_tables()
+    yield
+
+
+app = FastAPI(title="Task Manager API", lifespan=lifespan)
+
+app.include_router(tasks_router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
